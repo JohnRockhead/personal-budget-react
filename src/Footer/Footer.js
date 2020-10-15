c30cd39f32c8adf19a8b49cb631c04d05b048919
jrockhea <jrockhea@uncc.edu>
2020-10-14 22:34:49 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bottom">
+
+        <p className="center">All rights reserved &copy; John Rockhead</p>
+
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hero() {
+  return (
+    <div className="hero">
+    <h1>Personal Budget</h1>
+    <h2>A personal-budget management app</h2>
+    <img src="bg.png" alt="Clip art showing mobile devices use application" height="300px"/>
+</div>
+  );
+}
+
+export default Hero;
